c095ec21f9128462df4ad65f06e72cc1723dc691
Gabriel Souza <azevedoedit@gmail.com>
2022-05-19 23:30:05 -0300

    add routes for noticias service

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from services.noticias import Noticias
 
 app = Flask(__name__)
 
 @app.route('/status')
 def health_check():
     return "app is running"
+
+@app.route('/noticias', methods=["GET"])
+def list_noticias():
+    return Noticias.findAll()
+
+@app.route('/noticias/<id>', methods=["GET"])
+def find_noticias(id):
+    return Noticias.findById(id)
