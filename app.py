f840b2f5fc5dbc64f1dfb1758b91c66c4cd1cb3d
Gabriel Souza <azevedoedit@gmail.com>
2022-05-21 14:42:28 -0300

    add slash('/') at the end of app routes so it prevents 404

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from services.noticias import Noticias
 
 app = Flask(__name__)
 
-@app.route('/status')
+@app.route('/status/')
 def health_check():
     return "app is running"
 
-@app.route('/noticias', methods=["GET"])
+@app.route('/noticias/', methods=["GET"])
 def list_noticias():
     return Noticias.findAll()
 
-@app.route('/noticias/<id>', methods=["GET"])
+@app.route('/noticias/<id>/', methods=["GET"])
 def find_noticias(id):
     return Noticias.findById(id)
