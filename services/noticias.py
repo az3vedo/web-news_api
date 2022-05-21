46b7c1579253f8f7c0e46edd8e3d5f43b025a782
Gabriel Souza <azevedoedit@gmail.com>
2022-05-21 11:46:42 -0300

    fix noticias.findAll header content order

--- services/noticias.py
+++ services/noticias.py
@@ -6,8 +6,8 @@ from flask import jsonify
 class Noticias:
   def findAll():
     with closing(db_session):
-      query = db_session.query(Noticia.id, Noticia.titulo, Noticia.conteudo, Autor.nome, Assunto.nome, ).join(Autor, Assunto).all()
-      header = ["id","titulo","autor","assunto"]
+      query = db_session.query(Noticia.id, Noticia.titulo, Noticia.conteudo, Autor.nome, Assunto.nome).join(Autor, Assunto).all()
+      header = ["id","titulo", "conteudo", "autor","assunto"]
       response = []
       for result in query:
         # text = "id: {},titulo: {},conteudo: {},autor: {},assunto: {}".format(result[0], result[1], result[2], result[3], result[4])
@@ -21,4 +21,4 @@ class Noticias:
       response = []
       for result in query:
         response.append(dict(zip(header,result)))
-      return jsonify(response) 
+      return jsonify(response)
