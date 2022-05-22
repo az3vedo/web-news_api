1064cb218a69fabe64ebef0debed841f8a671c10
Eduarda-Oliveira <duda.oliveira.1503@gmail.com>
2022-05-22 19:51:48 -0300

    add db seed

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from services.noticias import Noticias
 
 app = Flask(__name__)
 
+@app.route('/')
+def seed():
+    Noticias.seed_db()
+    return "Banco populado"
+
 @app.route('/status/')
 def health_check():
     return "app is running"
